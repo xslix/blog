42b8c80cf357a0517f0775ff1821845fba3d0c9b
xslix <slifromtaraz@gmail.com>
2018-07-17 23:11:15 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  pure, withProps, withHandlers, compose, withState,
+} from 'recompose';
+import './App.css';
+
+const App = () => (
+  <div id="container">
+ 
+  </div>
+);
+
+export default enhanced(App);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+ 
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle-[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  mode: 'development',
+  devServer: {
+    hot: true,
+  },
+};
